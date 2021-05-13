440fa81d0a419d9533fa8f55204f17068d84db4e
pstefanou12 <patstefanou@gmail.com>
2021-05-13 16:33:49 -0400

    truncreg script appears to be working.

--- truncreg.R
+++ truncreg.R
@@ -7,13 +7,14 @@ RESULT_FILE <- '/result.csv'
 # Fetch command line arguments
 myArgs <- commandArgs(trailingOnly=TRUE)
 C <- as.numeric(myArgs[1]) # convert truncation parameter to numeric
-dir <- myArgs[2]  # truncation direction
-out_dir <- myArgs[3]
+dims <- as.numeric(myArgs[2])
+dir <- myArgs[3]  # truncation direction
+out_dir <- myArgs[4]
 
 # read in truncated data from the csv file
-d <- read.csv(paste(out_dir, TMP_FILE, sep=''), header=TRUE, col.names = c("NONE", "X0", "X1"))
-X <- as.matrix(d$X0)
-y <- as.matrix(d$X1)
+d <- read.csv(paste(out_dir, TMP_FILE, sep=''), header=TRUE)
+X <- as.matrix(d[,3:ncol(d)-1])
+y <- as.matrix(d[,ncol(d)])
 df <- data.frame(X=X, y=y)
 # truncated regression procedure
 trunc_reg <- truncreg(df$y ~ X, data=df, point=C, direction=dir, scaled=TRUE)
