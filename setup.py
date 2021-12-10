4f6adccd197e441aeadc187bd654696f24ef9902
Patroklos <patstefanou@gmail.com>
2021-12-10 09:51:54 -0500

    starting to add references to README :

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="delphi.ai",
-    version="0.1.7",
+    version="0.1.8",
     author="Patroklos Stefanou",
     author_email="patstefanou@gmail.com",
     description="Package for Robust Statistics",
