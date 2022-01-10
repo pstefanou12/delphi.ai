f4c03771fe595207f613d296a0e77db69d9bfd2c
Patroklos <patstefanou@gmail.com>
2022-01-10 17:04:54 -0500

    some small changes.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="delphi.ai",
-    version="0.2.1.2",
+    version="0.2.1.3",
     author="Patroklos Stefanou",
     author_email="patstefanou@gmail.com",
     description="Package for Robust Statistics",
