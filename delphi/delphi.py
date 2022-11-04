a13ea0d894386675b9c71aa35eb3d7cb5462e56f
Patroklos Stefanou <waterboi@stripe.com>
2022-11-04 07:25:31 -0700

    Removed the trainer, and now am creating a delphi model.

--- delphi/delphi.py
+++ delphi/delphi.py
@@ -4,10 +4,20 @@ Parent class for models to train in delphi trainer.
 
 
 import torch as ch
+from torch.optim import SGD, Adam, lr_scheduler
+import numpy as np
+import cox
+from cox.store import Store
+from time import time
+from tqdm import tqdm
+import copy
 import warnings
 from typing import Any
 
-from .utils.defaults import DELPHI_DEFAULTS, check_and_fill_args
+from .delphi import delphi
+from .utils.helpers import ckpt_at_epoch, AverageMeter, setup_store_with_metadata, Parameters
+from .utils.defaults import check_and_fill_args, TRAINER_DEFAULTS, DELPHI_DEFAULTS, check_and_fill_args
+from .utils import constants as consts
 
 # CONSTANTS 
 # default parameters for delphi module (can be overridden by any class
@@ -30,12 +40,31 @@ DEFAULTS = {
         'n_iter_no_change': (int, 5)
 }
 
+# CONSTANTS
+TRAIN = 'train'
+VAL = 'val'
+INFINITY = float('inf')
 
-class delphi:
+EVAL_LOGS_SCHEMA = {
+    'test_prec1':float,
+    'test_loss':float,
+    'time':float
+}
+
+# CONSTANTS 
+BY_ALG = 'by algorithm'  # default parameter depends on algorithm
+ADAM = 'adam'
+CYCLIC = 'cyclic'
+COSINE = 'cosine'
+LINEAR = 'linear'
+# default parameters for 
+
+
+class delphi(ch.nn.Module):
     '''
     Parent/abstract class for models to be passed into trainer.
     '''
-    def __init__(self, args): 
+    def __init__(self, args: Parameters, store: Store=None): 
         '''
         Args: 
             args (delphi.utils.helpers.Parameters) : parameter object holding hyperparameters
@@ -80,6 +109,21 @@ class delphi:
         self.params = None
         # algorithm optimizer and scheduler
         self._model= None
+
+        # keep track of the best model based off the best nll
+        best_loss, best_model = None, None
+
+        self.optimizer, self.schedule = None, None
+
+        self._model = self.model.model
+
+        # check and fill trainer hyperparameters
+        self.args = check_and_fill_args(args, TRAINER_DEFAULTS)
+        assert store is None or isinstance(store, cox.store.Store), "provided store is type: {}. expecting logging store cox.store.Store".format(type(store))
+        self.store = store 
+        
+        assert store is None or isinstance(store, cox.store.Store), "prorvided store is type: {}. expecting logging store cox.store.Store".format(type(store))
+        self.store = store
 
     def pretrain_hook(self) -> None:
         '''
@@ -144,6 +188,257 @@ class delphi:
         Regularizer method to apply to loss function. By default retruns 0.0.
         '''
         return ch.zeros(1, 1)
+
+
+
+    def make_optimizer_and_schedule(self):
+        """
+        Create optimizer (ch.nn.optim) and scheduler (ch.nn.optim.lr_scheduler module)
+        for SGD procedure. 
+        """
+        
+        """
+        TODO: change this
+        """
+        if self.model is None and self.params is None: raise ValueError('need to inititalize model or self.params')
+        # if cuda parameter provided, place model on GPU
+        if self.args.cuda: self._model.to('cuda')
+        # initialize optimizer, scheduler, and then get parameters
+        # default SGD optimizer
+        parameters = self.model.parameters()
+        self.optimizer = SGD(parameters, self.args.lr, momentum=self.args.momentum, weight_decay=self.args.weight_decay)
+        if self.args.custom_lr_multiplier == ADAM:  # adam
+            self.optimizer = Adam(parameters, lr=self.args.lr, weight_decay=self.args.weight_decay)
+        elif not self.args.constant: 
+            # setup learning rate scheduler
+            if self.args.custom_lr_multiplier == CYCLIC and self.M is not None: # cyclic
+                lr_func = lambda t: np.interp([t], [0, self.M*4//15, self.M], [0, 1, 0])[0]
+                self.schedule = lr_scheduler.LambdaLR(self.optimizer, lr_func)
+            # cosine annealing scheduler
+            elif self.args.custom_lr_multiplier == COSINE and self.M is not None:
+                schedule = lr_scheduler.CosineAnnealingLR(self.optimizer, self.M)
+            elif self.args.custom_lr_multiplier:
+                cs = self.args.custom_lr_multiplier
+                periods = eval(cs) if type(cs) is str else cs
+                # constant linear interpolation
+                if self.args.lr_interpolation == LINEAR:
+                    lr_func = lambda t: np.interp([t], *zip(*periods))[0]
+                # custom lr interpolation
+                else:
+                    def lr_func(ep):
+                        for (milestone, lr) in reversed(periods):
+                            if ep >= milestone: return lr
+                        return 1.0
+                self.schedule = lr_scheduler.LambdaLR(self.optimizer, lr_func)
+            # step learning rate
+            elif self.args.step_lr:
+                self.schedule = lr_scheduler.StepLR(self.optimizer, step_size=self.args.step_lr, 
+                gamma=self.args.step_lr_gamma)
+            
+        # if checkpoint load  optimizer and scheduler
+        if self.model.checkpoint:
+            self.optimizer.load_state_dict(self.model.checkpoint['optimizer'])
+            try:
+                schedule.load_state_dict(self.model.checkpoint['schedule'])
+            # if can't load scheduler state, take epoch steps
+            except:
+                steps_to_take = self.model.checkpoint['epoch']
+                print('Could not load schedule (was probably LambdaLR).'
+                    f' Stepping {steps_to_take} times instead...')
+                for i in range(steps_to_take):
+                    self.schedule.step() 
+        
+    def eval_model(self, loader):
+        """
+        Evaluate a model for standard (and optionally adversarial) accuracy.
+        Args:
+            loader (Iterable) : a dataloader serving batches from the test set
+            store (cox.Store) : store for saving results in (via tensorboardX)
+        Returns: 
+            schema with model performance metrics
+        """
+        # start timer
+        start_time = time.time()
+
+        # if store provided, 
+        if self.store is not None:
+            self.store.add_table('eval', consts.EVAL_LOGS_SCHEMA)
+
+        # add 
+        writer = self.store.tensorboard if self.store else None
+        test_prec1, test_loss = self.model_loop(VAL, loader, 1)
+
+        # log info 
+        if self.store:
+            log_info = {
+                'test_prec1': test_prec1,
+                'test_loss': test_loss,
+                'time': time.time() - start_time
+            }
+            self.store['eval'].append_row(log_info)
+        return log_info
+
+    def train_model(self, loaders):
+        """
+        Train model. 
+        Args: 
+            loaders (Iterable) : iterable with the train and validation set DataLoaders
+        Returns: 
+            Trained model
+        """
+        train_loader, val_loader = loaders
+        # check to make sure that the model's trainer has data in it
+        if len(train_loader.dataset) == 0: 
+            raise Exception('No Datapoints in Train Loader')
+        
+        if self.store is not None: 
+            self.store.add_table('logs', {
+                'trial': int,
+                'epoch': int,
+                'train_loss': float, 
+                'train_prec1': float,
+                'train_prec5': float, 
+                'val_loss': float,
+                'val_prec1': float, 
+                'val_prec5': float})
+            # record hyperparameters
+            setup_store_with_metadata(self.args, self.store)
+
+        best_loss, best_model = INFINITY, None
+        for trial in range(self.args.trials):
+            if self.args.verbose: print(f'trial: {trial + 1}')
+            t_start = time()
+            no_improvement_count = 0
+
+            # PRETRAIN HOOK
+            self.model.pretrain_hook()
+            
+            # make optimizer and scheduler for training neural network
+            self.make_optimizer_and_schedule()
+            
+            if self.model.checkpoint:
+                epoch = self.model.checkpoint['epoch']
+                best_prec1 = self.model.checkpoint['prec1'] if 'prec1' in self.model.checkpoint else self.model_loop(VAL, val_loader)[0]
+        
+            # do training loops until performing enough gradient steps or epochs
+            for epoch in range(1, self.args.epochs + 1):
+                # TRAIN LOOP
+                train_loss, train_prec1, train_prec5 = self.model_loop(TRAIN, train_loader, epoch)
+
+                                
+                # VALIDATION LOOP
+                if val_loader is not None:
+                    with ch.no_grad():
+                        val_loss, val_prec1, val_prec5 = self.model_loop(VAL, val_loader, epoch)
+                    
+                    if self.args.verbose: print(f'Epoch {epoch} - Loss: {val_loss}')
+
+                # if store provided, log epoch results
+                if self.store is not None:
+                    self.store['logs'].append_row({
+                        'trial': trial,
+                        'epoch': epoch,
+                        'train_loss': train_loss, 
+                        'train_prec1': train_prec1,
+                        'train_prec5': train_prec5,
+                        'val_loss': val_loss,
+                        'val_prec1': val_prec1, 
+                        'val_prec5': val_prec5})
+
+                # check for early completion
+                if self.args.early_stopping: 
+                    if self.args.tol > -INFINITY and ch.abs(val_loss - best_loss) <= self.args.tol:
+                        no_improvement_count += 1
+                    else: 
+                        no_improvement_count = 0
+
+                    if best_model is None or val_loss < best_loss: 
+                        best_model, best_loss = self.model.model[:], val_loss
+
+                    # model convergence
+                    if no_improvement_count >= self.args.n_iter_no_change:
+                        if self.args.verbose: 
+                            print("Convergence after %d epochs took %.2f seconds" % (epoch, time() - t_start))
+                        break
+                    
+            # POST TRAINING HOOK     
+            self.model.post_training_hook()
+            # update best model and best loss
+            if best_model is None or val_loss < best_loss: 
+                best_model, best_loss = copy.copy(self.model.model), val_loss 
+        
+        # inform user that SGD did not converge
+        if self.args.early_stopping and self.args.verbose and no_improvement_count < self.args.n_iter_no_change: 
+           print('Procedure did not converge after %d epochs and %.2f seconds' % (epoch, time() - t_start))
+        # set best model in delphi model object 
+        self.model.model = best_model        
+
+        return self.model
+                
+    def model_loop(self, loop_type, loader, epoch):
+        """
+        *Internal method* (refer to the train_model and eval_model functions for
+        how to train and evaluate models).
+        Runs a single epoch of either training or evaluating.
+        Args:
+            loop_type ('train' or 'val') : whether we are training or evaluating
+            loader (iterable) : an iterable loader
+            epoch (int) : which epoch we are currently on
+            writer : tensorboardX writer (optional)
+        Returns:
+            The average top1 accuracy and the average loss across the epoch.
+        """
+        # check loop type 
+        if not loop_type in ['train', 'val']: 
+            err_msg = "loop type must be in {0} must be 'train' or 'val".format(loop_type)
+            raise ValueError(err_msg)
+        # train or val loop
+        is_train = (loop_type == 'train')
+        loop_msg = 'Train' if is_train else 'Val'
+        
+        # if is_train, put model into train mode, else eval mode
+        loss_, prec1_, prec5_ = AverageMeter(), AverageMeter(), AverageMeter()
+        
+        # iterator
+        iterator = tqdm(enumerate(loader), total=len(loader), leave=False, bar_format='{l_bar}{bar:10}{r_bar}{bar:-10b}') if self.args.verbose else enumerate(loader) 
+        for i, batch in iterator:
+            self.optimizer.zero_grad()
+            loss, prec1, prec5 = self.model(batch)
+            
+            if len(loss.shape) > 0: loss = loss.sum()
+
+            # update average meters
+            loss_.update(loss)
+            if prec1 is not None: prec1_.update(prec1)
+            if prec5 is not None: prec5_.update(prec5)
+            # regularize
+            reg_term = self.model.regularize(batch)
+            if self.args.cuda:
+                reg_term = reg_term.cuda()
+            loss = loss + reg_term
+
+            # if training loop, perform training step
+            if is_train:
+                loss.backward()
+
+                self.model.pre_step_hook()
+
+                self.optimizer.step()
+                if self.schedule is not None and not self.model.args.epoch_step: self.schedule.step()
+            
+            # ITERATOR DESCRIPTION
+            if self.args.verbose:
+                desc = self.model.description(epoch, i, loop_msg, loss_, prec1_, prec5_, reg_term)
+                iterator.set_description(desc)
+ 
+            # ITERATION HOOK 
+            self.model.iteration_hook(i, loop_type, loss, prec1, prec5, batch)
+        if self.schedule is not None and self.model.args.epoch_step: self.scheduler.step() 
+        # EPOCH HOOK
+        self.model.epoch_hook(epoch, loop_type, loss, prec1, prec5)
+
+        return loss_.avg, prec1_.avg, prec5_.avg
+
 
     @property
     def model(self): 
