1562890a35d6f41a23c7e1ed748110076a0245ab
Patroklos Stefanou <waterboi@stripe.com>
2022-11-09 09:35:53 -0800

    Mistake in the condition.

--- delphi/delphi.py
+++ delphi/delphi.py
@@ -203,7 +203,7 @@ class delphi(ch.nn.Module):
         # initialize optimizer, scheduler, and then get parameters
         # default SGD optimizer
         # params = self.parameters if isinstance(self.parameters, list) else self.parameters.values()
-        params = self._parameters if self._parameters is None else [i[1] for i in self.named_parameters()]
+        params = self._parameters if self._parameters is not None else [i[1] for i in self.named_parameters()]
         if self.args.cuda: self.to('cuda')
         import pdb; pdb.set_trace()
         self.optimizer = SGD(params, lr=self.args.lr, momentum=self.args.momentum, weight_decay=self.args.weight_decay)
